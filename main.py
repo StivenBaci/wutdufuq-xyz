8d41ba49ac9721d4fe2842ac597d174686545b66
StivenBaci <stivenbaci@gmail.com>
2024-06-05 00:46:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import request
-from flask import Flask, jsonify, render_template
+from flask import request, Flask, jsonify, render_template
 import os
 import dmrv_html_generator as html_generator
+from datetime import datetime
+import requests
 
 
 app = Flask(__name__,
@@ -12,22 +13,40 @@ app = Flask(__name__,
 
 @app.route('/')
 def index():
+    log_ip()
     return render_template('index.html')
 
-@app.route('/gango44')
-def gango44():
-    return html_generator.getHTMLTable()
+# @app.route('/gango44')
+# def gango44():
+#     return html_generator.getHTMLTable()
 
-@app.route('/luckyluciano')
-def luckyluciano():
-    return html_generator.getProfileReport()
+# @app.route('/luckyluciano')
+# def luckyluciano():
+#     return html_generator.getProfileReport()
 
 @app.route("/get_ip", methods=["GET"])
 def get_ip():
+    return log_ip()
+
+def log_ip():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-        return request.environ['REMOTE_ADDR']
+        ip = request.environ['REMOTE_ADDR']
     else:
-        return request.environ['HTTP_X_FORWARDED_FOR']
+        ip = request.environ['HTTP_X_FORWARDED_FOR']
+    
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data = f"IP: {ip}, Timestamp: {timestamp}"
+    
+    # Generate a unique URL path
+    nfty_url = "https://nfty.sh/a8gd9jd7jdg5_hfd6hdf8h2gs9_8ghs5"
+    
+    # Send the data to the unique nfty.sh URL
+    response = requests.post(nfty_url, data={"text": data})
+    
+    if response.status_code == 200:
+        return ip
+    else:
+        return "Failed to send data", 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
